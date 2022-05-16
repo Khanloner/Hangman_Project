58845ffd5408bff557f3f687e94a63c145530b2c
Khanloner <90113862+Khanloner@users.noreply.github.com>
2022-05-16 18:58:57 +0530

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,44 @@
+hangman_art import stages, logo
+from hangman_words import word_list
+import random
+
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+print(logo)
+
+print(f'Pssst, the solution is {chosen_word}.')
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").
+    if guess in display:
+      print(f"You've already guessed {guess}")
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        
+        if letter == guess:
+            display[position] = letter
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+        
+      print(f"You guessed {guess}, that's not in the word. Yuo lose a life.")
+      lives -= 1
+      if lives == 0:
+        end_of_game = True
+        print("You lose.")    
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(stages[lives])
